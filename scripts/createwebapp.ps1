2226697e6918d51b1181875a7948d2b317db805d
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-20 06:16:30 -0700

    Update createwebapp.ps1

--- scripts/createwebapp.ps1
+++ scripts/createwebapp.ps1
@@ -1,9 +1,9 @@
-﻿$subsriptionid = Get-AutomationVariable -Name 'subsriptionid'
+$subsriptionid = Get-AutomationVariable -Name 'subsriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
 $Location = Get-AutomationVariable -Name 'Location'
 $Username = Get-AutomationVariable -Name 'Username'
 $Password = Get-AutomationVariable -Name 'Password'
 $appplan =  Get-AutomationVariable -Name 'appplan'
 $webapp =  Get-AutomationVariable -Name 'webapp'
-New-AzureRmAppServicePlan -Name "TestWebappplan999" -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
-New-AzureRmWebApp -Name "TestWebappp99" -AppServicePlan "TestWebappplan999" -ResourceGroupName $ResourceGroupName -Location $Location
+New-AzureRmAppServicePlan -Name $appplan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
+New-AzureRmWebApp -Name $webapp -AppServicePlan $appplan -ResourceGroupName $ResourceGroupName -Location $Location
