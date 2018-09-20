5bc8a660b41b861437aa48b7140e0acf0c49c2d2
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-20 06:26:56 -0700

    Update createwebapp.ps1

--- scripts/createwebapp.ps1
+++ scripts/createwebapp.ps1
@@ -5,5 +5,23 @@ $Username = Get-AutomationVariable -Name 'Username'
 $Password = Get-AutomationVariable -Name 'Password'
 $appplan =  Get-AutomationVariable -Name 'appplan'
 $webapp =  Get-AutomationVariable -Name 'webapp'
+
+Import-Module AzureRM.Resources
+Import-Module AzureRM.Profile
+Import-Module AzureRM.Websites
+Import-Module Azure
+Import-Module AzureRM.Automation
+
+    Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
+    Get-ExecutionPolicy -List
+    #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
+    $CredentialAssetName = 'DefaultAzureCredential'
+
+    #Get the credential with the above name from the Automation Asset store
+    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+    Add-AzureRmAccount -Environment 'AzureCloud' -Credential $Cred
+    Select-AzureRmSubscription -SubscriptionId $subsriptionid
+
 New-AzureRmAppServicePlan -Name $appplan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
 New-AzureRmWebApp -Name $webapp -AppServicePlan $appplan -ResourceGroupName $ResourceGroupName -Location $Location
