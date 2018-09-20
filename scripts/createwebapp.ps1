e1e9f3bf2f1ab74fbc7c09229eae03b91db71237
phanikumarsharma <ramaduguphanikumar@gmail.com>
2018-09-20 05:41:15 -0700

    json file and script files are committed

---
+++ scripts/createwebapp.ps1
@@ -0,0 +1,8 @@
+﻿$subsriptionid = Get-AutomationVariable -Name 'subsriptionid'
+$ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
+$Location = Get-AutomationVariable -Name 'Location'
+$Username = Get-AutomationVariable -Name 'Username'
+$Password = Get-AutomationVariable -Name 'Password'
+
+New-AzureRmAppServicePlan -Name "TestWebappplan999" -Location "centralus" -ResourceGroupName "TestAutomation-RG" -Tier Free
+New-AzureRmWebApp -Name "TestWebappp99" -AppServicePlan "TestWebappplan999" -ResourceGroupName "TestAutomation-RG" -Location "centralus"
