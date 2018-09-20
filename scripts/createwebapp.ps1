caf33376b5dcae8e8c099692640719ff8e92a439
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-20 06:55:17 -0700

    Update createwebapp.ps1

--- scripts/createwebapp.ps1
+++ scripts/createwebapp.ps1
@@ -5,15 +5,8 @@ $Username = Get-AutomationVariable -Name 'Username'
 $Password = Get-AutomationVariable -Name 'Password'
 $appplan =  Get-AutomationVariable -Name 'appplan'
 $webapp =  Get-AutomationVariable -Name 'webapp'
-    $CredentialAssetName = 'DefaultAzureCredential'
-
-    #Get the credential with the above name from the Automation Asset store
-    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
-    Login-AzureRmAccount -Environment 'AzureCloud' -Credential $Cred
-    Select-AzureRmSubscription -SubscriptionId $subsriptionid
-    $EnvironmentName = "AzureCloud"
 $Securepass=ConvertTo-SecureString -String $Password -AsPlainText -Force
 $Azurecred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($Username, $Securepass)
-$login=Login-AzureRmAccount -Credential $Azurecred -SubscriptionId $subscriptionid
+$login= Login-AzureRmAccount -Credential $Azurecred -SubscriptionId $subsriptionid
 New-AzureRmAppServicePlan -Name $appplan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
 New-AzureRmWebApp -Name $webapp -AppServicePlan $appplan -ResourceGroupName $ResourceGroupName -Location $Location
