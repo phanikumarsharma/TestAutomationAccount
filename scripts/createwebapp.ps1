116590f1a844a7aa531ceaed6cec7609df418419
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-20 22:39:42 -0700

    Update createwebapp.ps1

--- scripts/createwebapp.ps1
+++ scripts/createwebapp.ps1
@@ -1,13 +1,35 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @phanikumarsharma Sign out
+11
+7 25 Azure/RDS-Templates
+ Code  Issues 30  Pull requests 3  Projects 0  Wiki  Insights
+RDS-Templates/rdmi-peopletech/msft-rdmi-saas-offering/scripts/New-RdmiMgmtSetup.ps1
+0df0574  21 days ago
+@susmitapattanaik susmitapattanaik modified arm template and script and published files
+     
+304 lines (236 sloc)  14.5 KB
 $subsriptionid = Get-AutomationVariable -Name 'subsriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
 $Location = Get-AutomationVariable -Name 'Location'
+$ApplicationID = Get-AutomationVariable -Name 'ApplicationID'
+$RDBrokerURL = Get-AutomationVariable -Name 'RDBrokerURL'
+$ResourceURL = Get-AutomationVariable -Name 'ResourceURL'
+$fileURI = Get-AutomationVariable -Name 'fileURI'
 $Username = Get-AutomationVariable -Name 'Username'
 $Password = Get-AutomationVariable -Name 'Password'
-$fileURI = Get-AutomationVariable -Name 'fileURI'
+
+
 Invoke-WebRequest -Uri $fileURI -OutFile "C:\msft-rdmi-saas-offering.zip"
-New-Item -Path "C:\moduleszip" -ItemType directory -Force -ErrorAction SilentlyContinue
-Expand-Archive "C:\moduleszip.zip" -DestinationPath "C:\moduleszip" -ErrorAction SilentlyContinue
-$AzureModulesPath = Get-ChildItem -Path "C:\moduleszip\moduleszip"| Where-Object {$_.FullName -match 'AzureModules.zip'}
+New-Item -Path "C:\msft-rdmi-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
+Expand-Archive "C:\msft-rdmi-saas-offering.zip" -DestinationPath "C:\msft-rdmi-saas-offering" -ErrorAction SilentlyContinue
+$AzureModulesPath = Get-ChildItem -Path "C:\msft-rdmi-saas-offering\msft-rdmi-saas-offering"| Where-Object {$_.FullName -match 'AzureModules.zip'}
 Expand-Archive $AzureModulesPath.fullname -DestinationPath 'C:\Modules\Global' -ErrorAction SilentlyContinue
 
 Import-Module AzureRM.Resources
@@ -35,6 +57,11 @@ Import-Module AzureRM.Automation
        #> 
 
     $EnvironmentName = "AzureCloud"
+    $CodeBitPath= "C:\msft-rdmi-saas-offering\msft-rdmi-saas-offering"
+    $WebAppDirectory = ".\msft-rdmi-saas-web"
+    $WebAppExtractionPath = ".\msft-rdmi-saas-web\msft-rdmi-saas-web.zip"
+    $ApiAppDirectory = ".\msft-rdmi-saas-api"
+    $ApiAppExtractionPath = ".\msft-rdmi-saas-api\msft-rdmi-saas-api.zip"
     $AppServicePlan = "msft-rdmi-saas-$((get-date).ToString("ddMMyyyyhhmm"))"
     $WebApp = "RDmiMgmtWeb-$((get-date).ToString("ddMMyyyyhhmm"))"
     $ApiApp = "RDmiMgmtApi-$((get-date).ToString("ddMMyyyyhhmm"))"
@@ -42,6 +69,9 @@ Import-Module AzureRM.Automation
 
 try
 {
+    # Copy the files from github to VM
+    Import-Module AzureRM.Profile
+    Import-Module AzureRM.Resources
 
     ## RESOURCE GROUP ##
         Add-AzureRmAccount -Environment "AzureCloud" -Credential $Cred
@@ -75,12 +105,158 @@ try
                 Write-Output "Creating a WebApp in resource group  $ResourceGroupName ...";
                 New-AzureRmWebApp -Name $WebApp -Location $Location -AppServicePlan $AppServicePlan -ResourceGroupName $ResourceGroupName
                 Write-Output "WebApp with name $WebApp has been created"
+
+                ## CREATING API-APP ##
+
+                # Create an api app
+            
+                Write-Output "Creating a ApiApp in resource group  $ResourceGroupName ...";
+                $ServerFarmId = $AppPlan.Id
+                $propertiesobject = @{"ServerFarmId"= $ServerFarmId}
+                New-AzureRmResource -Location $Location -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites -ResourceName $ApiApp -Kind 'api' -ApiVersion 2016-08-01 -PropertyObject $propertiesobject -Force
+                Write-Output "ApiApp with name $ApiApp has been created"
             }
             catch [Exception]
             {
                 Write-Output $_.Exception.Message
             }
         
+        }
+        
+        if($ApiApp)
+        {
+            try
+            {
+
+                ## PUBLISHING API-APP PACKAGE ##
+                
+                Set-Location $CodeBitPath
+
+                # Extract the Api-App ZIP file content.
+            
+                Write-Output "Extracting the Api-App Zip File"
+                Expand-Archive -Path $ApiAppExtractionPath -DestinationPath $ApiAppDirectory -Force 
+                $ApiAppExtractedPath = Get-ChildItem -Path $ApiAppDirectory| Where-Object {$_.FullName -notmatch '\\*.zip($|\\)'} | Resolve-Path -Verbose
+                
+                # Get publishing profile from Api-App
+
+                Write-Output "Getting the Publishing profile information from Api-App"
+                $ApiAppXML = (Get-AzureRmWebAppPublishingProfile -Name $ApiApp `
+                -ResourceGroupName $ResourceGroupName  `
+                -OutputFile null)
+                $ApiAppXML = [xml]$ApiAppXML
+
+                # Extract connection information from publishing profile
+
+                Write-Output "Gathering the username, password and publishurl from the Web-App Publishing Profile"
+                $ApiAppUserName = $ApiAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userName").value
+                $ApiAppPassword = $ApiAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userPWD").value
+                $ApiAppURL = $ApiAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@publishUrl").value
+                
+                # Publish Api-App Package files recursively
+
+                Write-Output "Uploading the Extracted files to Api-App"
+                Get-ChildItem $ApiAppExtractedPath  | Compress-Archive -update -DestinationPath 'c:\msft-rdmi-saas-Api.zip' -Verbose 
+                test-path -path 'c:\msft-rdmi-saas-Api.zip'
+                $filePath = 'C:\msft-rdmi-saas-Api.zip'
+                $apiUrl = "https://$ApiAppURL/api/zipdeploy"
+                $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $ApiAppUserName, $ApiAppPassword)))
+                $userAgent = "powershell/1.0"
+                Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
+                Write-Output "Uploading of Extracted files to Api-App is Successful"
+
+                # Get Url of Web-App 
+
+                $GetWebApp = Get-AzureRmWebApp -Name $WebApp -ResourceGroupName $ResourceGroupName
+                $WebUrl = $GetWebApp.DefaultHostName 
+
+                # Adding App Settings to Api-App
+                
+                Write-Output "Adding App settings to Api-App"
+                $ApiAppSettings = @{"ApplicationId" = "$ApplicationID";
+                                    "RDBrokerUrl" = "$RDBrokerURL";
+                                    "ResourceUrl" = "$ResourceURL";
+                                    "RedirectURI" = "https://"+"$WebUrl"+"/";
+                                    }
+                <#$Redirecturl1="https://"+"$WebUrl"+"/"
+                $Redirecturl2="https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri="
+                $ADapplication=Get-AzureRmADApplication -ApplicationId $ApplicationID
+                $add=$ADapplication.ReplyUrls.Add($Redirecturl1)
+                $add=$ADapplication.ReplyUrls.Add("$Redirecturl2"+"$Redirecturl1")
+                $ReplyUrls=$ADapplication.ReplyUrls
+                Set-AzureRmADApplication -ApplicationId $ApplicationID -ReplyUrl $ReplyUrls #>
+                Set-AzureRmWebApp -AppSettings $ApiAppSettings -Name $ApiApp -ResourceGroupName $ResourceGroupName
+            }
+            catch [Exception]
+            {
+                Write-Output $_.Exception.Message
+            }
+        }
+        if($WebApp -and $ApiApp)
+        {
+            try
+            {
+                ## PUBLISHING WEB-APP PACKAGE ##
+                
+                Set-Location $CodeBitPath
+
+                Write-Output "Extracting the Web-App Zip File"
+ 
+                # Extract the Web-App ZIP file content.
+
+                Expand-Archive -Path $WebAppExtractionPath -DestinationPath $WebAppDirectory -Force 
+                $WebAppExtractedPath = Get-ChildItem -Path $WebAppDirectory| Where-Object {$_.FullName -notmatch '\\*.zip($|\\)'} | Resolve-Path -Verbose
+
+                # Get the main.bundle.js file Path 
+
+                $MainbundlePath = Get-ChildItem $WebAppExtractedPath -recurse | where {($_.FullName -match "main.bundle.js" ) -and ($_.FullName -notmatch "main.bundle.js.map")} | % {$_.FullName}
+ 
+                # Get Url of Api-App 
+
+                $GetUrl = Get-AzureRmResource -ResourceName $ApiApp -ResourceGroupName $ResourceGroupName -ExpandProperties
+                $GetApiUrl = $GetUrl.Properties | select defaultHostName
+                $ApiUrl = $GetApiUrl.defaultHostName
+
+                # Change the Url in the main.bundle.js file with the ApiURL
+
+                Write-Output "Updating the Url in main.bundle.js file with Api-app Url"
+                (Get-Content $MainbundlePath).replace( "[api_url]", "https://"+$ApiUrl) | Set-Content $MainbundlePath
+
+                # Get publishing profile from web app
+                
+                Write-Output "Getting the Publishing profile information from Web-App"
+                $WebAppXML = (Get-AzureRmWebAppPublishingProfile -Name $WebApp `
+                -ResourceGroupName $ResourceGroupName  `
+                -OutputFile null)
+
+                $WebAppXML = [xml]$WebAppXML
+
+                # Extract connection information from publishing profile
+
+                Write-Output "Gathering the username, password and publishurl from the Web-App Publishing Profile"
+                $WebAppUserName = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userName").value
+                $WebAppPassword = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userPWD").value
+                $WebAppURL = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@publishUrl").value
+             
+                # Publish Web-App Package files recursively
+
+                Write-Output "Uploading the Extracted files to Web-App"
+                Get-ChildItem $WebAppExtractedPath  | Compress-Archive -update  -DestinationPath 'c:\msft-rdmi-saas-web.zip' -Verbose 
+                test-path -path 'c:\msft-rdmi-saas-web.zip'
+                $filePath = 'C:\msft-rdmi-saas-web.zip'
+                $apiUrl = "https://$WebAppUrl/api/zipdeploy"
+                $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $WebAppUserName, $WebApppassword)))
+                $userAgent = "powershell/1.0"
+                Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
+                Write-Output "Uploading of Extracted files to Web-App is Successful"
+            }
+            catch [Exception]
+            {
+                Write-Output $_.Exception.Message
+            }
+
+            Write-Output "Api URL : https://$ApiUrl"
+            Write-Output "Web URL : https://$WebUrl"
         }
 }
 
@@ -89,5 +265,53 @@ catch [Exception]
     Write-Output $_.Exception.Message
 }
 
+New-PSDrive -Name RemoveAccount -PSProvider FileSystem -Root "C:\" | Out-Null
+@"
+Param(
+    [Parameter(Mandatory=`$True)]
+    [string] `$SubscriptionId,
+    [Parameter(Mandatory=`$True)]
+    [String] `$Username,
+    [Parameter(Mandatory=`$True)]
+    [string] `$Password,
+    [Parameter(Mandatory=`$True)]
+    [string] `$ResourceGroupName
+ 
+)
+Import-Module AzureRM.profile
+Import-Module AzureRM.Automation
+`$Securepass=ConvertTo-SecureString -String `$Password -AsPlainText -Force
+`$Azurecred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList(`$Username, `$Securepass)
+`$login=Login-AzureRmAccount -Credential `$Azurecred -SubscriptionId `$SubscriptionId
+Remove-AzureRmAutomationAccount -Name "msftsaas-autoAccount" -ResourceGroupName `$ResourceGroupName -Force 
+"@| Out-File -FilePath RemoveAccount:\RemoveAccount.ps1 -Force
 
-   
+    $CredentialAssetName = 'DefaultAzureCredential'
+
+    #Get the credential with the above name from the Automation Asset store
+    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+   login-AzureRmAccount -Environment 'AzureCloud' -Credential $Cred
+    Select-AzureRmSubscription -SubscriptionId $subsriptionid
+
+
+    $runbookName='removemsftsaasact'
+    $automationAccountName="msftsaas-autoAccount"
+    #Create a Run Book
+    $AAcctRunbook=New-AzureRmAutomationRunbook -Name $runbookName -Type PowerShell -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationAccountName
+
+    #Import modules to Automation Account
+    $modules="AzureRM.profile,Azurerm.compute,azurerm.resources"
+    $modulenames=$modules.Split(",")
+    foreach($modulename in $modulenames){
+    Set-AzureRmAutomationModule -Name $modulename -AutomationAccountName $automationAccountName -ResourceGroupName $ResourcegroupName
+    }
+
+    #Importe powershell file to Runbooks
+    Import-AzureRmAutomationRunbook -Path "C:\RemoveAccount.ps1" -Name $runbookName -Type PowerShell -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName -Force
+
+    #Publishing Runbook
+    Publish-AzureRmAutomationRunbook -Name $runbookName -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName
+
+    #Providing parameter values to powershell script file
+    $params=@{"UserName"=$UserName;"Password"=$Password;"ResourcegroupName"=$ResourcegroupName;"SubscriptionId"=$subsriptionid}
+    Start-AzureRmAutomationRunbook -Name $runbookName -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName -Parameters $params
