8b675690e8624f5d6250e224945cf62fd3a932f4
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-17 11:50:27 +0530

    Update newresourcegroup.ps1

--- scripts/newresourcegroup.ps1
+++ scripts/newresourcegroup.ps1
@@ -1,3 +1,14 @@
+Import-Module AzureRM.Resources
+Import-Module AzureRM.Profile
+Import-Module AzureRM.Websites
+Import-Module Azure
+Import-Module AzureRM.Automation
+Import-Module AzureAD
+
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
+Get-ExecutionPolicy -List
+
 $subscriptionid = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
 $Location = Get-AutomationVariable -Name 'Location'
