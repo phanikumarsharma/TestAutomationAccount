84f805bd77be0353e69c50c25a02e7a24862c77e
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-17 11:30:11 +0530

    Delete newresourcegroup.ps1

--- newresourcegroup.ps1
+++
@@ -1,7 +0,0 @@
-$subscriptionid = Get-AutomationVariable -Name 'subscriptionid'
-$ResourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
-$Location = Get-AutomationVariable -Name 'Location'
-$Username = Get-AutomationVariable -Name 'Username'
-$Password = Get-AutomationVariable -Name 'Password'
-
-New-AzureRmResourceGroup -Name RG01 -Location "South Central US"
