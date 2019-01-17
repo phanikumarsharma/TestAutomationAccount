577a47e7e88feea3093c5767d0d45f701eef341e
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-17 16:08:28 +0530

    Update newresourcegroup.ps1

--- scripts/newresourcegroup.ps1
+++ scripts/newresourcegroup.ps1
@@ -1,15 +1,16 @@
 Import-Module AzureRM.profile
 Import-Module AzureRM.Automation
 
+$TenantId = Get-AutomationVariable -Name 'TenantId'
 $subscriptionid = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
 $Location = Get-AutomationVariable -Name 'Location'
-$Username = Get-AutomationVariable -Name 'Username'
-$Password = Get-AutomationVariable -Name 'Password'
+$ApplicationId = Get-AutomationVariable -Name 'ApplicationId'
+$AppSecretKey = Get-AutomationVariable -Name 'AppSecretKey'
 
-$CredentialAssetName = 'DefaultAzureCredential'
-#Get the credential with the above name from the Automation Asset store
-$Cred = Get-AutomationPSCredential -Name $CredentialAssetName
-Login-AzureRmAccount -Credential $Cred
 Select-AzureRmSubscription -SubscriptionId $subscriptionid
+$securePassword = $AppSecretKey | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
+Login-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId $TenantId
+
 New-AzureRmResourceGroup -Name "RG01" -Location "South Central US"
