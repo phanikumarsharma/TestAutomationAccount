1048ac20377a022851885a085e16981d2703d272
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-17 12:05:47 +0530

    Update newresourcegroup.ps1

--- scripts/newresourcegroup.ps1
+++ scripts/newresourcegroup.ps1
@@ -1,13 +1,5 @@
-Import-Module AzureRM.Resources
-Import-Module AzureRM.Profile
-Import-Module AzureRM.Websites
-Import-Module Azure
+Import-Module AzureRM.profile
 Import-Module AzureRM.Automation
-Import-Module AzureAD
-
-Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
-Get-ExecutionPolicy -List
 
 $subscriptionid = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
@@ -18,6 +10,6 @@ $Password = Get-AutomationVariable -Name 'Password'
 $CredentialAssetName = 'DefaultAzureCredential'
 #Get the credential with the above name from the Automation Asset store
 $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
-Add-AzureRmAccount -Environment 'AzureCloud' -Credential $Cred
+Login-AzureRmAccount -Credential $Cred
 Select-AzureRmSubscription -SubscriptionId $subsriptionid
-New-AzureRmResourceGroup -Name RG01 -Location "South Central US"
+New-AzureRmResourceGroup -Name "RG01" -Location "South Central US"
