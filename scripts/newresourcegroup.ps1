2f8e84cf0ec85ffa5f55a417d9cfe220277a625c
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-22 14:53:19 +0530

    Update newresourcegroup.ps1

--- scripts/newresourcegroup.ps1
+++ scripts/newresourcegroup.ps1
@@ -6,13 +6,14 @@ $accountName = Get-AutomationVariable -Name 'accountName'
 $applicationDisplayName = Get-AutomationVariable -Name 'applicationDisplayName'
 $applicationId = Get-AutomationVariable -Name 'applicationId'
 $appSecretKey = Get-AutomationVariable -Name 'appSecretKey'
+<#
 $securePassword = $appSecretKey | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($applicationId, $securePassword)
 Connect-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $tenantId
-
+#>
 $userPassword = ConvertTo-SecureString -String $securePassword -AsPlainText -Force
 $userCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $userPassword
-Add-AzureRmAccount -TenantId $tenantid -ServicePrincipal -SubscriptionId $subscriptionId -Credential $userCredential
+Login-AzureRmAccount -TenantId $tenantid -ServicePrincipal -SubscriptionId $subscriptionId -Credential $userCredential
 
 Get-AzureRmResourceGroup
 
