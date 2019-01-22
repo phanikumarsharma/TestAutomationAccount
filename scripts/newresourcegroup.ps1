a29838881c8ca205ecc77a485b90402793c7f367
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-01-22 14:46:21 +0530

    Update newresourcegroup.ps1

--- scripts/newresourcegroup.ps1
+++ scripts/newresourcegroup.ps1
@@ -10,4 +10,8 @@ $securePassword = $appSecretKey | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($applicationId, $securePassword)
 Connect-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $tenantId
 
+$userPassword = ConvertTo-SecureString -String $securePassword -AsPlainText -Force
+$userCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $userPassword
+Add-AzureRmAccount -TenantId $tenantid -ServicePrincipal -SubscriptionId $subscriptionId -Credential $userCredential
+
  
